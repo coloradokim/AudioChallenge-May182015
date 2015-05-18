b9b53461af039d6971536cf63d24c26a3c0991e3
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-05-18 12:51:21 -0600

    Added function to generate random images

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var button = document.getElementById("button");
 var scream = document.getElementById("scream");
 var image = document.getElementById("image");
 var imageArray = ["scream-one.jpg","scream-two.jpg","scream-three.jpg","scream-four.jpg"];
-var i = 0;
+
 button.addEventListener('mousedown',function(){
   scream.play();
-  image.src=""+imageArray[i]+"";
+  image.src=""+imageArray[shuffle(imageArray)]+"";
 })
 
 button.addEventListener('mouseup',function(){
   scream.pause();
   image.src="";
-  i++;
 })
+
+var shuffle = function(array) {
+ var newIndex = Math.floor(Math.random() * array.length);
+ return newIndex;
+};
