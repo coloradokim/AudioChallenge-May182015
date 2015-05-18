cc8c58df8ce6edeab3c71b032904854b9ff43907
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-05-18 12:57:52 -0600

    added more images

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var button = document.getElementById("button");
 var scream = document.getElementById("scream");
 var image = document.getElementById("image");
-var imageArray = ["scream-one.jpg","scream-two.jpg","scream-three.jpg","scream-four.jpg"];
+var imageArray = ["scream-one.jpg","scream-two.jpg","scream-three.jpg","scream-four.jpg", "scream-five.jpg", "scream-six.jpg", "scream-seven.jpg", "scream-eight.jpg", "scream-nine.jpg"];
 
 button.addEventListener('mousedown',function(){
   scream.play();
